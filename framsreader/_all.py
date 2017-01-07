7d59e2f0d3b81eaeacca93bcdbec3f916957c27b
michał <kempka.michal@gmail.com>
2017-01-07 19:20:25 +0100

    Added some error messages

--- framsreader/_all.py
+++ framsreader/_all.py
@@ -3,6 +3,8 @@ import warnings
 import os.path
 from collections import defaultdict
 
+warnings.simplefilter('always', UserWarning)
+
 _INT_FLOAT_REGEX = r'([+|-]?(?:0|[1-9]\d*))(\.\d+)?([eE][-+]?\d+)?'
 _NATURAL_REGEX = r'(?:0|[1-9]\d*)'
 _HEX__NUMBER_REGEX = r'[+|-]?0[xX][\da-fA-F]*'
@@ -17,14 +19,23 @@ _FRAMSCRIPT_XML_PATH = os.path.join((os.path.dirname(__file__)), "framscript.xml
 _AVAILABLE_CONTEXTS = ["expdef", "show", "style", "neuro", "script", "gen", "sim", "expt"]
 
 # Messages:
-# TODO
 _NO_FILE_EXTENSION_WARNING = "No file extension found. Setting default context."
 _UNSUPPORTED_EXTENSION_WARNING = "Unsupported file extension: '{}'. Setting default context."
+_UNEXPECTED_KEY_WARNING = "Unexpected key encountered: key: '{}', class: '{}', context: '{}' )"
+_NOT_A_NUMBER_ERROR = "Expression cannot be parsed to a number: {}"
+_MULTILINE_NOT_CLOSED_ERROR = "Multiline property for key: '{}' was not closed with '~'."
+_STRING_NOT_CLOSED_ERROR = "String expression not closed with '~'"
+_EMPTY_SERIALIZED_ERROR = "Empty value for '@Serialized' not allowed."
+_NO_OBJECT_ERROR = "No object defined for the current line."
+_XYZ_ERROR = "XYZ format should look like this: XYZ[ a,b,c], Got: '{}'"
+_REFERENCE_FORMAT_ERROR = "reference sign '^' should be followed by an integer. Got: {}"
+_COLON_EXPECTED_ERROR = "Colon ':' was expected. Got: {}"
 
 
 def _create_specs_from_xml():
+    specs = dict()
     # TODO read specs from framscript.xml to resolve ambiguities
-    return dict()
+    return specs
 
 
 _specs = _create_specs_from_xml()
@@ -45,35 +56,38 @@ def _str_to_number(s):
 
     except ValueError:
         pass
-    # TODO msg
-    raise ValueError()
+    raise ValueError(_NOT_A_NUMBER_ERROR.format(s))
 
 
 def parse_value(value, classname=None, key=None, context=None):
     assert isinstance(value, str)
-    if (classname, key, context) in _specs:
-        raise NotImplementedError()
-    else:
-        if value.startswith("@Serialized:"):
-            prop = value.split(":", 1)[1]
-            prop = deserialize(prop)
-            return prop
-        else:
-            try:
-                parsed_number = _str_to_number(value)
-                return parsed_number
-            except ValueError:
-                pass
 
-            return value.strip()
+    if (context, classname) in _specs:
+        spec = _specs[(context, classname)]
+        if key in spec:
+            raise NotImplementedError()
+        else:
+            warnings.warn(_UNEXPECTED_KEY_WARNING.format(key, classname, context))
+
+    if value.startswith("@Serialized:"):
+        prop = value.split(":", 1)[1]
+        prop = deserialize(prop)
+        return prop
+    else:
+        try:
+            parsed_number = _str_to_number(value)
+            return parsed_number
+        except ValueError:
+            pass
+
+        return value.strip()
 
 
 def _extract_string(exp):
     exp = exp[1:]
     str_end_match = _re.search(_QUOTE_REGEX, exp)
     if str_end_match is None:
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_STRING_NOT_CLOSED_ERROR.format(exp))
     str_end = str_end_match.span()[0]
     s = exp[:str_end]
     reminder = exp[str_end + 1:]
@@ -95,29 +109,25 @@ def _extract_number(exp):
 def _extract_xyz(exp):
     exp = exp.strip()
     if not exp.startswith('XYZ['):
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_XYZ_ERROR.format(exp))
     exp = exp[4:]
     x, exp = _extract_number(exp)
     x = float(x)
     exp = exp.strip()
     if exp[0] != ',':
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_XYZ_ERROR.format(exp))
     exp = exp[1:]
     y, exp = _extract_number(exp)
     y = float(y)
     exp = exp.strip()
     if exp[0] != ',':
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_XYZ_ERROR.format(exp))
     exp = exp[1:]
     z, exp = _extract_number(exp)
     z = float(z)
     exp = exp.strip()
     if exp[0] != ']':
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_XYZ_ERROR.format(exp))
     return (x, y, z), exp[1:]
 
 
@@ -125,8 +135,7 @@ def _extract_reference(exp):
     exp = exp[1:].strip()
     i_match = _re.match(_NATURAL_REGEX, exp)
     if i_match is None:
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_REFERENCE_FORMAT_ERROR.format(exp))
     else:
         end_i = i_match.span()[1]
         ref_index = int(exp[:end_i])
@@ -171,8 +180,7 @@ def _extract_custom_object(exp):
 def deserialize(expression):
     stripped_exp = expression.strip()
     if stripped_exp == '':
-        # TODO msg
-        raise ValueError('Empty value for "@Serialized" not allowed.')
+        raise ValueError(_EMPTY_SERIALIZED_ERROR)
     # Just load with json ...
 
     if stripped_exp == 'null':
@@ -191,14 +199,12 @@ def deserialize(expression):
     while len(exp) > 0:
         current_object_is_reference = False
         if main_object_determined and len(objects) == 0:
-            # TODO msg
-            raise ValueError()
+            raise ValueError(_NO_OBJECT_ERROR)
         if expect_dict_value:
             if exp[0] == ':':
                 exp = exp[1:].strip()
             else:
-                # TODO msg
-                raise ValueError()
+                raise ValueError(_COLON_EXPECTED_ERROR.foramt(exp[0]))
         # List continuation
         # TODO support for XYZ tuples
         if exp[0] == ",":
@@ -356,27 +362,27 @@ def loads(s, context=None):
             parsing_error = True
             break
     if multiline_key is not None:
-        # TODO msg
-        raise ValueError()
+        raise ValueError(_MULTILINE_NOT_CLOSED_ERROR.format(multiline_key))
     if parsing_error:
         raise ValueError("Parsing error. Incorrect syntax in line {}:\n{}".format(line_num, line))
 
     return objects
 
 
-def load(filename):
+def load(filename, context=None):
     file = open(filename)
-    try:
-        _, ext = filename.split(".")
-        if ext not in _AVAILABLE_CONTEXTS:
-            context = None
-            warnings.warn(_UNSUPPORTED_EXTENSION_WARNING.format(ext))
-        else:
-            context = ext
+    if context is None:
+        try:
+            _, ext = filename.split(".")
+            if ext not in _AVAILABLE_CONTEXTS:
+                context = None
+                warnings.warn(_UNSUPPORTED_EXTENSION_WARNING.format(ext))
+            else:
+                context = ext
 
-    except RuntimeError:
-        warnings.warn(_NO_FILE_EXTENSION_WARNING)
-        context = None
+        except RuntimeError:
+            warnings.warn(_NO_FILE_EXTENSION_WARNING)
+            context = None
     s = file.read()
     file.close()
     return loads(s, context=context)
